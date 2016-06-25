679e9d88350d8cbda6962cc6489950b5d3ccbcc5
Brian Ding <brian_ding@outlook.com>
2016-06-25 10:23:58 +0800

    Commit

--- SoPDF/IO/PdfWriter.cs
+++ SoPDF/IO/PdfWriter.cs
@@ -1,6 +1,3 @@
-using SoPDF.Core;
-using SoPDF.Objects;
-using System;
 using System.IO;
 using System.Text;
 
@@ -8,66 +5,11 @@ namespace SoPDF.IO
 {
     internal class PdfWriter : BinaryWriter
     {
-        internal PdfWriter(string path) : base(new FileStream(path, FileMode.Create), Encoding.UTF8, false) { }
+        internal PdfWriter(string path) : base(new FileStream(path, FileMode.Create), Encoding.ASCII, false) { }
 
         internal void Write(IWritable writableObj)
         {
             base.Write(writableObj.ToBytes());
         }
-
-        //internal void Write(PdfHeader header)
-        //{
-        //    base.Write(header.ToBytes());
-        //}
-
-        //internal void Write(ArrayObject arrayObj)
-        //{
-        //    base.Write(arrayObj.ToBytes());
-        //}
-
-        //internal void Write(BoolObject boolObj)
-        //{
-        //    base.Write(boolObj.ToBytes());
-        //}
-
-        //internal void Write(DictionaryObject dictionaryObj)
-        //{
-        //    base.Write(dictionaryObj.ToBytes());
-        //}
-
-        //internal void Write(IntObject intObj)
-        //{
-        //    base.Write(intObj.ToBytes());
-        //}
-
-        //internal void Write(NameObject nameObj)
-        //{
-        //    base.Write(nameObj.ToBytes());
-        //}
-
-        //internal void Write(NumObject numObject)
-        //{
-        //    base.Write(numObject.ToBytes());
-        //}
-
-        //internal void Write(PdfObject pdfObj)
-        //{
-        //    base.Write(pdfObj.ToBytes());
-        //}
-
-        //internal void Write(RealObject realObj)
-        //{
-        //    base.Write(realObj.ToBytes());
-        //}
-
-        //internal void Write(StreamObject streamObj)
-        //{
-        //    base.Write(streamObj.ToBytes());
-        //}
-
-        //internal void Write(StringObject stringObj)
-        //{
-        //    base.Write(stringObj.ToBytes());
-        //}
     }
 }
